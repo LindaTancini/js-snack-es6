5629321c8e49a1440175c16a129e8a1f2b459c88
LindaTancini <lindatancini95@gmail.com>
2025-03-07 15:16:35 +0100

    feat: create const "footballTeam" with objects

--- snack2/main.js
+++ snack2/main.js
@@ -1,1 +1,11 @@
-console.log("Ciao Linda");
+// CREO ARRAY CON OGGETTI
+const footballTeam = [
+  { nome: "Dynamo Tigers", punti: 0, falli: 0 },
+  { nome: "Falcon United", punti: 0, falli: 0 },
+  { nome: "Thunder Strikers", punti: 0, falli: 0 },
+  { nome: "Shadow Warriors", punti: 0, falli: 0 },
+];
+console.log(footballTeam); // Vedo il contenuto dell'Array
+console.log(footballTeam[1].nome); // esce il nome della squadra Falcon United, se era 0 usciva Dynamo Tigers ecc...
+console.log(footballTeam[1].punti); // escono i punti della squadra Falcon United, se era 0 uscivano quelli di Dynamo Tigers ecc...
+console.log(footballTeam[1].falli); // escono i falli della squadra Falcon United, se era 0 uscivano quelli di Dynamo Tigers ecc...
