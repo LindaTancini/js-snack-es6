2c3dcbdf391fe357b9bc9ad99667a17ac37fda3e
LindaTancini <lindatancini95@gmail.com>
2025-03-07 14:37:07 +0100

    feat: create const "bicycle" with objects

--- snack1/main.js
+++ snack1/main.js
@@ -1,1 +1,13 @@
 console.log("Ciao Linda");
+
+// CREO ARRAY CON OGGETTI
+
+const bicycle = [
+  { nome: "Matteo", peso: 2.35 },
+  { nome: "Aurora", peso: 1.89 },
+  { nome: "Luigi", peso: 3.12 },
+  { nome: "Sofia", peso: 2.78 },
+];
+console.log(bicycle); // Vedo il contenuto dell'Array
+console.log(bicycle[1].nome); // esce Aurora, se era 0 usciva Matteo ecc...
+console.log(bicycle[1].peso); // esce peso di Aurora, se era 0 usciva quello di Matteo ecc...
