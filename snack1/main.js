63da2ebe2565fbf25dcb1eccc5e60549901fd20b
LindaTancini <lindatancini95@gmail.com>
2025-03-07 14:59:19 +0100

    feat: added snack1 with for cycle

--- snack1/main.js
+++ snack1/main.js
@@ -1,7 +1,4 @@
-console.log("Ciao Linda");
-
 // CREO ARRAY CON OGGETTI
-
 const bicycle = [
   { nome: "Matteo", peso: 2.35 },
   { nome: "Aurora", peso: 1.89 },
@@ -11,3 +8,19 @@ const bicycle = [
 console.log(bicycle); // Vedo il contenuto dell'Array
 console.log(bicycle[1].nome); // esce Aurora, se era 0 usciva Matteo ecc...
 console.log(bicycle[1].peso); // esce peso di Aurora, se era 0 usciva quello di Matteo ecc...
+
+// CALCOLO IL PESO MINORE
+let min = 0;
+for (let i = 0; i < bicycle.length; i++) {
+  console.log(bicycle[i].peso); // con bicycle[i] prendo il peso delle bici
+  console.log(bicycle[min].peso); // con bicycle[min].peso prendo il peso della bici meno pesante
+  if (bicycle[i].peso < bicycle[min].peso) {
+    min = i; // min conterrà ora l'indice della bici più leggera
+  }
+}
+
+// STAMPO IN CONSOLE
+console.log(bicycle[min].peso);
+console.log(
+  `La bicicletta più leggera è di ${bicycle[min].nome} con un peso di ${bicycle[min].peso}`
+);
